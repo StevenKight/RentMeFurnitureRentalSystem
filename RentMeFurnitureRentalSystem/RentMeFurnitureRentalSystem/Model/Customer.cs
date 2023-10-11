6c8545ab03c5a9701818f850d7be7d918aa76351
Steven Kight <sgk0711@gmail.com>
2023-10-11 15:04:42 -0400

    Create Customer model class.

---
+++ RentMeFurnitureRentalSystem/RentMeFurnitureRentalSystem/Model/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentMeFurnitureRentalSystem.Model
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Gender { get; set; }
+        public string Phone { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zipcode { get; set; }
+        public DateTime RegistrationDate { get; set; }
+    }
+}
